2fb55838b07a22535430098bcd68686ae1276fd8
Logan Metzger <loganmetzger@Logan-Metzger.local>
2020-07-10 12:17:52 -0400

    imported axios, created variables for container div and API data, made axios get request to confirm it's working

--- components/Cards.js
+++ components/Cards.js
@@ -20,5 +20,14 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
- import axios from 'axios'
- 
+ import axios from 'axios';
+ const cardsContainer = document.querySelector('.cards-container');
+ const articleData = 'https://lambda-times-backend.herokuapp.com/articles';
+
+ axios.get(articleData)
+ .then( (response) => {
+     console.log('OH YEAH')
+ })
+ .catch( (error) => {
+     console.log(error.dir)
+ })
